513e3fb4845323e46a6fd3055169815d2c98b1cb
Jal <jalilputer@hotmail.com>
2017-07-28 12:05:49 -0300

    master2

--- Mp3Web/src/main/java/com/maxi/model/ListaTemas.java
+++ Mp3Web/src/main/java/com/maxi/model/ListaTemas.java
@@ -1,8 +1,10 @@
 package com.maxi.model;
 
 import java.io.File;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class ListaTemas {
 	
@@ -10,10 +12,10 @@ public class ListaTemas {
 	
 	public ListaTemas(){
 		
-		String directorio = "C:\\Users\\mpadulao\\workspace\\Mp3Web\\src\\main\\webapp\\mp3";
+		String directorio = "C://Users//jalputer//Documents//GitHub//Aplicacion//Aplicacion//Mp3Web//src//main//webapp//mp3";
+		
 		File file = new File(directorio);
 		File[] temas = file.listFiles();
-		
 		
 		for(int i=0;i<temas.length;i++){
 			
